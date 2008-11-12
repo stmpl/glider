bcb3f4839ab59c015435c5fc1d9ecdbad13f831d
marcoow <marco.otte-witte@simplabs.com>
2008-11-12 15:20:00 +0100

    dramatically improved the script (also [#2 state:resolved])

--- src/glider.js
+++ src/glider.js
@@ -1,101 +1,118 @@
 var Glider = Class.create({
 
-  working: false,
-
   initialize: function(container, options, callback) {
     this.container = $(container);
     this.callback = callback;
-    this.options = $H({ direction: 'x', transition: Effect.Transitions.sinoidal }).merge(options);
+    this.options = $H({ direction: 'x', transition: Effect.Transitions.sinoidal, duration: .6 }).merge(options);
     this.sectionsContainer = this.container.down('.sections');
-    this.sections = this.sectionsContainer.select('.section');
+    this.sectionInfo = this.collectSectionInfo();
     this.container.setStyle({
-      height: this.sections.first().getHeight() + 'px'
+      height: this.sectionInfo.first().section.getHeight() + 'px',
+      width: this.sectionInfo.first().section.getWidth() + 'px'
     });
-    this.current = { section: this.sections[0], index: 0 };
-    this.setInitial();
+    this.setInitialSection();
   },
   
-  setInitial: function() {
+  collectSectionInfo: function() {
+    counter = -1;
+    sections = this.sectionsContainer.select('.section');
+    return sections.map(function(section) {
+      counter++;
+      return {
+        section: $(section),
+        index: counter,
+        left: function() {
+          left = 0;
+          for (i = 0; i < counter; i++) {
+            left += sections[i].getWidth();
+          }
+          return left;
+        }.bind(this)(),
+        top: function() {
+          top = 0;
+          for (i = 0; i < counter; i++) {
+            top += sections[i].getHeight();
+          }
+          return top;
+        }.bind(this)()
+      }
+    }.bind(this));
+  },
+
+  setInitialSection: function() {
     initial = null;
-    if ((initialSection = this.sectionsContainer.select('.initial')).length == 1) {
+    if (document.location.href.indexOf('#') > 0) {
+      initial = this.sectionInfo.find(function(sectionInfo) {
+        return sectionInfo.section.id == document.location.href.split('#').last();
+      });
+      initial = initial ? initial.section : null;
+    }
+    if (!initial && (initialSection = this.sectionsContainer.select('.initial')).length == 1) {
       initial = initialSection[0];
     }
-    if (document.location.href.indexOf('#') > 0) {
-      initial = this.findSection(document.location.href.split('#').last());
+    if (!initial) {
+      initial = this.sectionInfo.first().section;
     }
-    if (initial) {
-      this.show(initial.id, 0.0000000000001);
-    }
+    this.show(initial.id, 0.0000000000001);
   },
 
-  findIndex: function(section) {
-    index = 0;
-    this.sections.each(function(s) {
-      if (section.id == s.id) {
-        throw $break;
-      }
-      index++;
-    });
-    return index;
-  },
-  
-  findSection: function(section) {
-    return this.sections.find(function(s) {
-      return s.id == section;
+  getSectionInfo: function(id) {
+    return this.sectionInfo.find(function(sectionInfo) {
+      return sectionInfo.section.id == id;
     });
   },
 
-  calculateEffects: function() {
+  calculateEffects: function(sectionInfo, duration) {
     effects = [];
-    direction = (this.current.index < index) ? -1 : 1;
-    offsetX = 0;
-    offsetY = 0;
-    if (this.current.index < index) {
-      for (i = this.current.index; i < index; i++) {
-        offsetX += this.sections[i].getWidth();
-        offsetY += this.sections[i].getHeight();
-      }
-    } else if (this.current.index > index) {
-      for (i = index; i < this.current.index; i++) {
-        offsetX += this.sections[i].getWidth();
-        offsetY += this.sections[i].getHeight();
-      }
+    movements = { x: 0, y: 0 }
+    scales =    { x: false, y: false, factor: 1}
+    if (this.options.get('direction') == 'x') {
+      direction = (this.sectionsContainer.positionedOffset().left + sectionInfo.left) > 0 ? -1 : 1;
+      movements.x   = (Math.abs(this.sectionsContainer.positionedOffset().left + sectionInfo.left) * direction)
+      scales.factor = sectionInfo.section.getWidth() / this.container.getWidth();
+      scales.x      = true;
+    } else {
+      direction = (this.sectionsContainer.positionedOffset().top + sectionInfo.top) > 0 ? -1 : 1;
+      movements.y   = (Math.abs(this.sectionsContainer.positionedOffset().top + sectionInfo.top) * direction)
+      scales.factor = sectionInfo.section.getHeight() / this.container.getHeight();
+      scales.y      = true;
     }
     effects.push(new Effect.Move(this.sectionsContainer, {
-      x: (this.options.get('direction') == 'x' ? (offsetX * direction) : 0),
-      y: (this.options.get('direction') == 'y' ? (offsetY * direction) : 0),
-      transition: this.options.get('transition')
+      duration: duration,
+      x: movements.x,
+      y: movements.y,
+      transition: this.options.get('transition'),
+      queue: { scope: 'simplabs:glider' }
     }));
-    if (this.options.get('direction') == 'x') {
-      factor = this.sections[index].getWidth() / this.current.section.getWidth();
-      scaleOptions = { scaleX: true, scaleY: false, scaleContent: false }
-    } else {
-      factor = this.sections[index].getHeight() / this.current.section.getHeight();
-      scaleOptions = { scaleX: false, scaleY: true, scaleContent: false }
+    if (scales.factor != 1) {
+      effects.push(new Effect.Scale(this.container, scales.factor * 100, {
+        duration: duration,
+        scaleX: scales.x,
+        scaleY: scales.y,
+        scaleContent: false,
+        queue: { scope: 'simplabs:glider' }
+      }));
     }
-    effects.push(new Effect.Scale(this.container, factor * 100, scaleOptions));
     return effects;
   },
 
-  show: function(section, duration) {
-    section = this.findSection(section);
-    if (section == undefined) return;
-    if (this.working) return;
-    this.working = true;
-    index = this.findIndex(section);
-    effects = this.calculateEffects();
-    new Effect.Parallel(effects, {
-      duration: (duration || .6),
-      afterFinish: function() {
-        this.working = false;
-      }.bind(this)
-    });
-    this.current = { section: section, index: index}
-    if (this.options.get('onSlide')) {
-      this.options.get('onSlide')(section);
+  show: function(sectionId, duration) {
+    duration = duration || this.options.get('duration');
+    Effect.Queues.get('simplabs:glider').each(function(effect) { effect.cancel(); });
+    sectionInfo = this.getSectionInfo(sectionId);
+    if (sectionInfo == undefined) {
+      return;
     }
-  }
-
+    effects = this.calculateEffects(sectionInfo, duration);
+    new Effect.Parallel(effects, {
+      duration: duration,
+      queue: { scope: 'simplabs:glider' }
+    });
+    if (this.options.get('onSlide')) {
+      this.options.get('onSlide')(sectionInfo.section);
+    }
+  },
+  
 });
 
 var GliderLink = Class.create({
@@ -108,7 +125,7 @@ var GliderLink = Class.create({
 
   onclick: function(event) {
     Event.stop(event);
-    this.glider.show(this.link.href.split('#')[1]);
+    this.glider.show(this.link.href.split('#').last());
   }
 
 });
